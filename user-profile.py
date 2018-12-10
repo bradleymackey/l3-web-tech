979718ee154a9ca8e009cf19444b2377506fe7f8
Bradley Mackey <me@bradleymackey.com>
2018-12-10 21:51:47 +0000

    working internal rating predictor system, ready to be implemented into interface now

--- user-profile.py
+++ user-profile.py
@@ -2,11 +2,20 @@ import pandas as pd
 import numpy as np
 from scipy.sparse.linalg import svds
 
+class RatingPredictor(object):
+    """
+    loads files from disk and performs ratings predictions for a given user
+    """
+
+    def __init__(self):
+        pass
+
 # users.csv is where we maintain all the user profiles
 users_data = pd.read_csv("ml-latest-small/users.csv")
 
-
 ratings_data = pd.read_csv("ml-latest-small/ratings.csv")
+ratings_data = ratings_data.append(users_data, ignore_index=False, sort=True)
+print(ratings_data.tail())
 movie_names = pd.read_csv("ml-latest-small/movies.csv")
 movie_links = pd.read_csv("ml-latest-small/links.csv")
 
@@ -24,15 +33,13 @@ R_df = movie_data.pivot(index="userId",columns="movieId",values="rating").fillna
 print(R_df.head())
 R = R_df.values # numpy array
 user_ratings_mean = np.mean(R,axis=1)
-print(user_ratings_mean)
 R_demeaned = R - user_ratings_mean.reshape(-1,1)
 
 # doing the estimate to find out who is the most likley to like each movie
 U, sigma, Vt = svds(R_demeaned, k=50) # k = heuristic based on how many latent factors we want to consider
-print(U.shape)
-print(sigma.shape)
-print(Vt.shape)
+sigma = np.diag(sigma)
 all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1,1)
-preds_df = pd.DataFrame(all_user_predicted_ratings, columns=R_df.columns)
-print(preds_df.head())
+preds_df = pd.DataFrame(all_user_predicted_ratings, columns=R_df.columns, index=R_df.index)
+
+print(preds_df.filter(axis=0, regex="^crackio weener$"))
 
